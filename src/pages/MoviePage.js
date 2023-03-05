3c061adf0bc7c6430b1e03db0d050d0c38a0c4d0
OanhL1B <vuthihongoanh1803@gmail.com>
2023-03-05 15:08:20 +0700

    8. Page Movie, Search, button search

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import useSWR from "swr";
+import MovieCard from "../components/movie/MovieCard";
 const MoviePage = () => {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const [movies, setMovies] = useState([]);
+
+  const { data } = useSWR(
+    `https://api.themoviedb.org/3/movie/popular?api_key=4942b98510b1078ce139cb7667bf7765`,
+    fetcher
+  );
+  useEffect(() => {
+    if (data && data.results) setMovies(data.results);
+  }, [data]);
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nisi
-      odit esse optio libero debitis sit, earum magnam eos! Sit aperiam
-      praesentium ipsum. Nemo, deserunt. Magnam rem accusantium ipsum
-      distinctio!
+    <div className="py-10 page-container">
+      <div className="flex mb-10">
+        <div className="flex-1">
+          <input
+            type="text"
+            className="w-full p-4 bg-slate-800 text-white outline-none"
+            placeholder="Type here to search"
+          />
+        </div>
+        <button className="p-4 bg-primary text-white">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="grid grid-cols-4 gap-10">
+        {movies.length > 0 &&
+          movies.map((item) => (
+            <MovieCard key={item.id} item={item}></MovieCard>
+          ))}
+      </div>
     </div>
   );
 };
