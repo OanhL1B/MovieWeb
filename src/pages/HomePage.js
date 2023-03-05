4b2b939d8abf6b5b117f1e9c1cc466589b7e1dee
OanhL1B <vuthihongoanh1803@gmail.com>
2023-03-05 12:32:08 +0700

    7. Router,  active homepage and movies

---
+++ src/pages/HomePage.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from "react";
+
+import MovieList from "../components/movie/MovieList";
+
+const HomePage = () => {
+  return (
+    <Fragment>
+      <section className="movie-layout page-container pb-20 ">
+        <h2 className="capitalize text-white mb-10 text-2xl font-bold">
+          Now playing
+        </h2>
+        <MovieList></MovieList>
+      </section>
+      <section className="movie-layout page-container pb-20 ">
+        <h2 className="capitalize text-white mb-10 text-2xl font-bold">
+          Top rating
+        </h2>
+        <MovieList type="top_rated"></MovieList>
+      </section>
+      <section className="movie-layout page-container pb-20 ">
+        <h2 className="capitalize text-white mb-10 text-2xl font-bold">
+          Trending
+        </h2>
+        <MovieList type="popular"></MovieList>
+      </section>
+    </Fragment>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/MoviePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MoviePage = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nisi
+      odit esse optio libero debitis sit, earum magnam eos! Sit aperiam
+      praesentium ipsum. Nemo, deserunt. Magnam rem accusantium ipsum
+      distinctio!
+    </div>
+  );
+};
+
+export default MoviePage;
