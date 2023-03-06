fd8e48694fe53f34763d7b8bd656dabd83b68283
OanhL1B <vuthihongoanh1803@gmail.com>
2023-03-06 11:40:34 +0700

    15. pagination use react-pagination

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from "react";
-
+import ReactPaginate from "react-paginate";
 import useSWR from "swr";
 import MovieCard from "../components/movie/MovieCard";
 import useDebounce from "../hooks/useDebounce";
 //https://api.themoviedb.org/3/search/movie?api_key=4942b98510b1078ce139cb7667bf7765
-const pageCount = 5;
+
+// số lượng phần tử hiện thị trên 1 trang
+const itemsPerPage = 20;
 const MoviePage = () => {
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+
+  // -------------------------------------
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const [nextPage, setNextPage] = useState(1);
   const [filter, setFilter] = useState("");
@@ -21,6 +27,7 @@ const MoviePage = () => {
   // nếu không có data và không có lỗi sẽ cho loading
   const loading = !data && !error;
 
+  // search
   useEffect(() => {
     if (filterDebounce) {
       setUrl(
@@ -32,11 +39,20 @@ const MoviePage = () => {
       );
     }
   }, [filterDebounce, nextPage]);
-  // if (!data) return null;
+
   const movies = data?.results || [];
-  // const { page, total_pages } = data;
-  // console.log("page", page);
-  // console.log("page", total_pages);
+  // pagiantion
+  useEffect(() => {
+    if (!data || !data.total_pages) return;
+    setPageCount(Math.ceil(data.total_pages / itemsPerPage));
+  }, [data, itemOffset]);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % data.total_pages;
+    setItemOffset(newOffset);
+    setNextPage(event.selected + 1);
+  };
   return (
     <div className="py-10 page-container">
       <div className="flex mb-10">
@@ -77,50 +93,17 @@ const MoviePage = () => {
           ))}
       </div>
       {/* pagination */}
-      <div className="flex item-center justify-center mt-10 gap-x-5">
-        <span className="cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
-            onClick={() => setNextPage(nextPage - 1)}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 19.5L8.25 12l7.5-7.5"
-            />
-          </svg>
-        </span>
-        {new Array(pageCount).fill(0).map((item, index) => (
-          <span
-            className="cursor-pointer inline-block text-slate-900 py-2 px-4 rounded bg-white leading-none"
-            onClick={() => setNextPage(index + 1)}
-          >
-            {index + 1}
-          </span>
-        ))}
-
-        <span className="cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
-            onClick={() => setNextPage(nextPage + 1)}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M8.25 4.5l7.5 7.5-7.5 7.5"
-            />
-          </svg>
-        </span>
+      <div className="mt-10">
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel="next >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="< previous"
+          renderOnZeroPageCount={null}
+          className="pagination"
+        />
       </div>
     </div>
   );
