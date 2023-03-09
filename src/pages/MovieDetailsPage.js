12e925266a91c8515bec58b340bfe616b07f4555
OanhL1B <vuthihongoanh1803@gmail.com>
2023-03-09 16:15:25 +0700

    20. MovieDetailt

--- src/pages/MovieDetailsPage.js
+++ src/pages/MovieDetailsPage.js
@@ -55,114 +55,197 @@ const MovieDetailsPage = () => {
       <p className="text-center text-sm leading-relaxed max-w-[600px] mx-auto mb-10">
         {overview}
       </p>
-      <MovieCredits></MovieCredits>
-      <MovieVideos></MovieVideos>
-      <MovieSimilar></MovieSimilar>
+      <MovieMeta type="credits"></MovieMeta>
+      <MovieMeta type="videos"></MovieMeta>
+      <MovieMeta type="similar"></MovieMeta>
     </div>
   );
 };
 
-function MovieCredits() {
+function MovieMeta({ type = "videos" }) {
   const { movieId } = useParams();
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   // https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=<<api_key>>
-  const { data, error } = useSWR(
-    tmdbAPI.getMovieMeta(movieId, "credits"),
-    fetcher
-  );
+  const { data, error } = useSWR(tmdbAPI.getMovieMeta(movieId, type), fetcher);
   if (!data) return null;
-  const { cast } = data;
-  if (!cast || cast.length <= 0) return null;
-  return (
-    <div className="py-10">
-      <h2 className="text-center text-3xl mb-10">Casts</h2>
-      <div className="mx-20">
-        <div className="grid grid-cols-4 gap-x-5">
-          {/* splice cắt đổ thành 1 mảng mới 4 người */}
-          {cast.splice(0, 4).map((item) => (
-            <div className="cast-item" key={item.id}>
-              <img
-                src={tmdbAPI.imageOriginal(item.profile_path)}
-                className="w-full h-[350px] object-cover rounded-lg mb-3"
-                alt=""
-              />
-              <h3 className="text-xl font-medium">{item.name}</h3>
-            </div>
-          ))}
+  if (type === "credits") {
+    const { cast } = data;
+    if (!cast || cast.length <= 0) return null;
+    return (
+      <div className="py-10">
+        <h2 className="text-center text-3xl mb-10">Casts</h2>
+        <div className="mx-20">
+          <div className="grid grid-cols-4 gap-x-5">
+            {/* splice cắt đổ thành 1 mảng mới 4 người */}
+            {cast.splice(0, 4).map((item) => (
+              <div className="cast-item" key={item.id}>
+                <img
+                  src={tmdbAPI.imageOriginal(item.profile_path)}
+                  className="w-full h-[350px] object-cover rounded-lg mb-3"
+                  alt=""
+                />
+                <h3 className="text-xl font-medium">{item.name}</h3>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
-
-function MovieVideos() {
-  const { movieId } = useParams();
-
-  const fetcher = (...args) => fetch(...args).then((res) => res.json());
-  // https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=<<api_key>>
-  const { data, error } = useSWR(
-    tmdbAPI.getMovieMeta(movieId, "videos"),
-    fetcher
-  );
-  if (!data) return null;
-  const { results } = data;
-  if (!results || results.length <= 0) return null;
-  //   console.log(data);
-
-  return (
-    <div className="py-10">
-      <div className="flex flex-col gap-10">
-        {results.splice(0, 2).map((item) => (
-          <div key={item.id}>
-            <h3 className="mb-3 text-xl font-medium p-3 bg-secondary inline-block">
-              {item.name}
-            </h3>
-            <div className="w-full aspect-video">
-              <iframe
-                width="942"
-                height="530"
-                src={`https://www.youtube.com/embed/${item.key}`}
-                title="Thị Mầu - Hòa Minzy x Masew | Official Music Video"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-                className="w-full h-full object-fill"
-              ></iframe>
-            </div>
+    );
+  } else {
+    const { results } = data;
+    if (!results || results.length <= 0) return null;
+    if (type === "videos")
+      return (
+        <div className="py-10">
+          <div className="flex flex-col gap-10">
+            {results.splice(0, 2).map((item) => (
+              <div key={item.id}>
+                <h3 className="mb-3 text-xl font-medium p-3 bg-secondary inline-block">
+                  {item.name}
+                </h3>
+                <div className="w-full aspect-video">
+                  <iframe
+                    width="942"
+                    height="530"
+                    src={`https://www.youtube.com/embed/${item.key}`}
+                    title="Thị Mầu - Hòa Minzy x Masew | Official Music Video"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen
+                    className="w-full h-full object-fill"
+                  ></iframe>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+      );
+    if (type === "similar")
+      return (
+        <div className="py-10">
+          <h2 className="text-3xl font-medium mb-10">Similar Movies</h2>
+          <div className="movie-list">
+            <Swiper
+              grabCursor={"true"}
+              spaceBetween={40}
+              slidesPerView={"auto"}
+            >
+              {results.map((item) => (
+                <SwiperSlide key={item.id}>
+                  <MovieCard item={item}></MovieCard>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </div>
+      );
+  }
+  return null;
 }
 
-function MovieSimilar() {
-  const { movieId } = useParams();
+// function MovieCredits() {
+//   const { movieId } = useParams();
 
-  const fetcher = (...args) => fetch(...args).then((res) => res.json());
-  // https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key=<<api_key>>
-  const { data, error } = useSWR(
-    tmdbAPI.getMovieMeta(movieId, "similar"),
-    fetcher
-  );
+//   const fetcher = (...args) => fetch(...args).then((res) => res.json());
+//   // https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=<<api_key>>
+//   const { data, error } = useSWR(
+//     tmdbAPI.getMovieMeta(movieId, "credits"),
+//     fetcher
+//   );
+//   if (!data) return null;
+//   const { cast } = data;
+//   if (!cast || cast.length <= 0) return null;
 
-  if (!data) return null;
-  const { results } = data;
-  if (!results || results <= 0) return null;
-  return (
-    <div className="py-10">
-      <h2 className="text-3xl font-medium mb-10">Similar Movies</h2>
-      <div className="movie-list">
-        <Swiper grabCursor={"true"} spaceBetween={40} slidesPerView={"auto"}>
-          {results.map((item) => (
-            <SwiperSlide key={item.id}>
-              <MovieCard item={item}></MovieCard>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="py-10">
+//       <h2 className="text-center text-3xl mb-10">Casts</h2>
+//       <div className="mx-20">
+//         <div className="grid grid-cols-4 gap-x-5">
+//           {/* splice cắt đổ thành 1 mảng mới 4 người */}
+//           {cast.splice(0, 4).map((item) => (
+//             <div className="cast-item" key={item.id}>
+//               <img
+//                 src={tmdbAPI.imageOriginal(item.profile_path)}
+//                 className="w-full h-[350px] object-cover rounded-lg mb-3"
+//                 alt=""
+//               />
+//               <h3 className="text-xl font-medium">{item.name}</h3>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// function MovieVideos() {
+//   const { movieId } = useParams();
+
+//   const fetcher = (...args) => fetch(...args).then((res) => res.json());
+//   // https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=<<api_key>>
+//   const { data, error } = useSWR(
+//     tmdbAPI.getMovieMeta(movieId, "videos"),
+//     fetcher
+//   );
+//   if (!data) return null;
+//   const { results } = data;
+//   if (!results || results.length <= 0) return null;
+//   //   console.log(data);
+
+//   return (
+//     <div className="py-10">
+//       <div className="flex flex-col gap-10">
+//         {results.splice(0, 2).map((item) => (
+//           <div key={item.id}>
+//             <h3 className="mb-3 text-xl font-medium p-3 bg-secondary inline-block">
+//               {item.name}
+//             </h3>
+//             <div className="w-full aspect-video">
+//               <iframe
+//                 width="942"
+//                 height="530"
+//                 src={`https://www.youtube.com/embed/${item.key}`}
+//                 title="Thị Mầu - Hòa Minzy x Masew | Official Music Video"
+//                 frameBorder="0"
+//                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+//                 allowFullScreen
+//                 className="w-full h-full object-fill"
+//               ></iframe>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// function MovieSimilar() {
+//   const { movieId } = useParams();
+
+//   const fetcher = (...args) => fetch(...args).then((res) => res.json());
+//   // https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key=<<api_key>>
+//   const { data, error } = useSWR(
+//     tmdbAPI.getMovieMeta(movieId, "similar"),
+//     fetcher
+//   );
+
+//   if (!data) return null;
+//   const { results } = data;
+//   if (!results || results <= 0) return null;
+//   return (
+//     <div className="py-10">
+//       <h2 className="text-3xl font-medium mb-10">Similar Movies</h2>
+//       <div className="movie-list">
+//         <Swiper grabCursor={"true"} spaceBetween={40} slidesPerView={"auto"}>
+//           {results.map((item) => (
+//             <SwiperSlide key={item.id}>
+//               <MovieCard item={item}></MovieCard>
+//             </SwiperSlide>
+//           ))}
+//         </Swiper>
+//       </div>
+//     </div>
+//   );
+// }
 export default MovieDetailsPage;
